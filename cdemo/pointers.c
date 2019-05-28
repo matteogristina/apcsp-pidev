16b10b0938ebcef91b12f664d6020d1e8f7e2d85
matteogristina <33038970+matteogristina@users.noreply.github.com>
2019-05-27 20:22:03 -0700

    Update pointers.c

--- cdemo/pointers.c
+++ cdemo/pointers.c
@@ -17,4 +17,16 @@ int main()
   printf("The value of ptrtoa is %d\n", ptrtoa);
   printf("It stores the value %d\n", *ptrtoa);
   printf("The address of a is %d\n", &a);
+  
+  float d = 0.203;
+  float e = 1.13;
+  float* ptrtod = &d;
+  float* ptrtoe = &e;
+  printf("d: %f has addr: %d\n", d, &d);
+  printf("e: %f has addr: %d\n", e, &e);
+  float temp = *ptrtod;
+  *ptrtod = *ptrtoe;
+  *ptrtoe = temp;
+  printf("new d = %f\n", d);
+  printf("new e = %f\n", e);
 }
