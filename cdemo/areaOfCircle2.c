12e11006bd142bea1e8832b94ec9f8a330fa090f
Matteo <matteogristina@gmail.com>
2019-05-28 15:46:47 +0000

    add areaofcircle2 again

--- cdemo/areaOfCircle2.c
+++ cdemo/areaOfCircle2.c
@@ -9,20 +9,14 @@ float areaOfCircle(float radius)
 
 int main(int argc, char* argv[])
 {
-  int arg = 0;
-  while (arg < argc)
-  {
-    printf("Bound %d is %s\n", arg, argv[arg]);
-    arg++;
-  }
-  float lower, upper;
+  float smaller, larger;
   printf("Enter a small radius:\n");
-  scanf("%f", &lower);
+  scanf("%f", &smaller);
   printf("Enter a larger radius:\n");
-  scanf("%f", &upper);
-  for (float radius = lower; radius <= upper; radius++)
+  scanf("%f", &larger);
+  for (float radius = smaller; radius <= larger; radius++)
     {
 	float solution = areaOfCircle(radius);
-	printf("Area of circle when r = %f: %f\n", radius, solution);
+	printf("Area of circle when r = %f is approx. %f\n", radius, solution);
     }
 }
